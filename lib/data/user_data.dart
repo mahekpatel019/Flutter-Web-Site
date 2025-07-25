1511f8fb4b6925a41dd110774c3b630dd9d87434
Mahek Patel <patelmahek788@gmail.com>
2025-07-25 14:06:34 +0530

    25-07-2025

--- lib/data/user_data.dart
+++ lib/data/user_data.dart
@@ -1,11 +1,11 @@
 import 'package:web_site/models/user_model.dart';
 
 final List<UserModel> users = const [
-    UserModel(name: 'Alice Johnson', email: 'alice@gmail.com'),
-    UserModel(name: 'Bob Smith', email: 'bob@yahoo.com'),
-    UserModel(name: 'Charlie Brown', email: 'charlie@outlook.com'),
-    UserModel(name: 'Diana Prince', email: 'diana@amazon.com'),
-    UserModel(name: 'Ethan Hunt', email: 'ethan@mission.com'),
-    UserModel(name: 'Fiona Glenanne', email: 'fiona@burnnotice.com'),
-    UserModel(name: 'Jhon Doe', email: 'jhondoe@yahoo.com'),
-  ];
+  UserModel(name: 'Alice Johnson', email: 'alice@gmail.com'),
+  UserModel(name: 'Bob Smith', email: 'bob@yahoo.com'),
+  UserModel(name: 'Charlie Brown', email: 'charlie@outlook.com'),
+  UserModel(name: 'Diana Prince', email: 'diana@amazon.com'),
+  UserModel(name: 'Ethan Hunt', email: 'ethan@mission.com'),
+  UserModel(name: 'Fiona Glenanne', email: 'fiona@burnnotice.com'),
+  UserModel(name: 'Jhon Doe', email: 'jhondoe@yahoo.com'),
+];
