3f045ed09386f6be1f4f5185a2957559497e97ce
Mahek Patel <patelmahek788@gmail.com>
2025-07-26 15:45:16 +0530

    26-07-2025

--- lib/widgets/chart_widgets/candlestic_chart_widget.dart
+++ lib/widgets/chart_widgets/candlestic_chart_widget.dart
@@ -8,7 +8,6 @@ class SalesCandlestickChartWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //Custom chart container
     return ChartContainer(
       child: CandlestickChart(
         CandlestickChartData(
